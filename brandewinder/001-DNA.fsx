958c97a285861acceed9e11257ebc965d63f3820
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-03-15 10:30:15 -0700

    Problem 1 and 2 with Kim and Dan

---
+++ brandewinder/001-DNA.fsx
@@ -0,0 +1,26 @@
+﻿(*
+Teamwork with Dan and Kim, at the San Francisco F# Coding Breakfast
+on March 12, 2014: http://www.meetup.com/sfsharp/events/166845672/
+*)
+
+let input = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+let output = "20 12 17 21"
+
+let explicitVersion (dna:string) =
+    input 
+    |> Seq.countBy (fun x -> x)
+    |> Seq.sortBy (fun (letter,_) -> letter)
+    |> Seq.map (fun (_,count) -> count)
+    |> Seq.map (fun x -> string x)
+    |> String.concat " "
+
+printfn "Verification: %b" (explicitVersion input = output)
+
+let compactVersion (dna:string) =
+    input 
+    |> Seq.countBy id
+    |> Seq.sortBy fst 
+    |> Seq.map (snd >> string)
+    |> String.concat " "
+
+printfn "Verification: %b" (compactVersion input = output)
---
+++ brandewinder/002-RNA.fsx
@@ -0,0 +1,20 @@
+﻿(*
+Teamwork with Dan and Kim, at the San Francisco F# Coding Breakfast
+on March 12, 2014: http://www.meetup.com/sfsharp/events/166845672/
+*)
+
+let input = "GATGGAACTTGACTACGTAAATT"
+let output = "GAUGGAACUUGACUACGUAAAUU"
+
+open System
+let firstVersion (rna:string) = rna.Replace('T','U')
+
+printfn "Verification: %b" (firstVersion input = output)
+
+// Now without using System - not quite correct just yet.
+let secondVersion (rna:string) = 
+    rna 
+    |> Seq.map (fun x -> if x = 'T' then 'U' else x) 
+    |> string
+
+printfn "Verification: %b" (secondVersion  input = output)
